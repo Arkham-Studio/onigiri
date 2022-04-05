a68683e2c2c078c0d120c66dcc504435daedb980
AkhamStudio <arkhamstudio.dev@gmail.com>
2022-04-05 22:54:48 +0200

    fix : variables

---
+++ Runtime/Variables/Vector3IntVariable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Arkham.Onigiri.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Vector3")]
+    public class Vector3IntVariable : BaseVariable<Vector3Int>
+    {
+        public override int ValueToInt() => Mathf.RoundToInt(Value.magnitude);
+        public override float ValueToFloat() => Value.magnitude;
+        public override bool ValueToBool() => Value.magnitude > 0;
+        public override string ValueToString() => Value.ToString();
+
+
+        public float magnitude { get => Value.magnitude; }
+        public float sqrMagnitude { get => Value.sqrMagnitude; }
+    }
+}
---
+++ Runtime/Variables/Vector4Variable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Arkham.Onigiri.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Vector3")]
+    public class Vector4Variable : BaseVariable<Vector4>
+    {
+        public override int ValueToInt() => Mathf.RoundToInt(Value.magnitude);
+        public override float ValueToFloat() => Value.magnitude;
+        public override bool ValueToBool() => Value.magnitude > 0;
+        public override string ValueToString() => Value.ToString();
+
+        public float magnitude { get => Value.magnitude; }
+        public float sqrMagnitude { get => Value.sqrMagnitude; }
+        public Vector4 normalized { get => Value.normalized; }
+    }
+}
